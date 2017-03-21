970ead495e45a4ad97be817738849cf8dd395be7
GitOnion <b93703028@ntu.edu.tw>
2017-03-21 11:57:16 -0700

    create random bmp

--- random_rgb_bmp_gen.py
+++ random_rgb_bmp_gen.py
@@ -3,22 +3,32 @@ import png
 import random
 
 
-def random_png_gen(myemail="myemail@gmail.com"):
+def random_png_gen(myemail, width, height):
     """
     Use the python built-in random module to reduce the usage of randor.org quota.
     (The quota has only 1mn bits, a single grey scale bmp pixel is 8 bits. if I am
     to generate 128 * 128 * 3 * 8 bits from random.org, two trials would drain it.)
     """
     rand_from_random_org = RandomIntGen(myemail).generate(256, 1, 255, 1, 10)
+    pic = []
+    for i in range(height):
+        row = []
+        for i in range(width):
+            row += create_pixel(rand_from_random_org)
+        pic.append(tuple(row))
 
+    f = open('random.png', 'wb')
+    w = png.Writer(128, 128)
+    w.write(f, pic)
+    f.close()
 
-
-    # f = open('random.png', 'wb')
-    # w = png.Writer(128, 128)
-    # w.write(f, pic)
-    # f.close()
-
-def 
+def create_pixel(randomList):
+    pixel = []
+    for i in range(3):
+        color = random.sample(randomList,1)[0]
+        color = (color + random.randint(0, 255)) % 255
+        pixel.append(color)
+    return pixel
 
 if __name__ == "__main__":
-    random_png_gen()
+    random_png_gen("myemail@gmail.com", 128, 128)
